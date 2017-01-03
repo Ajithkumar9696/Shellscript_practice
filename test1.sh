058395d36ea5049d34a2fa1ae298709b1b7d1691
Ashok kumar <avasarla@visteon.com>
2017-01-03 18:35:43 +0530

    [base commit] shell script base commit
    Signed-off-by: Ashok kumar <avasarla@visteon.com>

---
+++ test1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "test";
+echo "Number of args" $#
+for ARGS in $*
+do
+  echo "arguments" $ARGS;
+done
+echo "argument1 " $1
+echo "argument2 " $2
+echo "file name " $0
+echo "last operatrion status" $?
+val=`expr 2 + 2 `
+echo "Total value : $val"
+a=1
+b=10
+if [ $# -gt 0 ]
+then
+  echo "arguments there "
+fi
+if [ $a -eq $b ]
+then
+  echo "equal"
+else
+  echo "not equal"
+fi
+while [ $a -lt $b ]
+do
+  echo "loop"
+  a=`expr $a + 1`
+  break 4
+done
+umount $1* >/dev/null
+sleep 1
+dd if=/dev/zero of=$1 bs=4096  count=1
+sleep 1
+(echo n;echo p;echo 1;echo;echo +64MB;echo n;echo p;echo 2;echo;echo;echo p;echo w) | fdisk $1
